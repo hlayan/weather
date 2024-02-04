62427b9b43156317d1eb082b106356ed61a637bd
Hlayan Htet Aung <hlayanhtetaung@gmail.com>
2024-02-05 05:03:53 +0630

    Borrow colors from NowInAndroid

--- core/designsystem/src/main/java/com/hlayan/weather/core/designsystem/theme/Shape.kt
+++
@@ -1,5 +0,0 @@
-package com.hlayan.weather.core.designsystem.theme
-
-import androidx.compose.material3.Shapes
-
-val Shapes = Shapes()
--- core/designsystem/src/main/java/com/hlayan/weather/core/designsystem/theme/Type.kt
+++
@@ -1,5 +0,0 @@
-package com.hlayan.weather.core.designsystem.theme
-
-import androidx.compose.material3.Typography
-
-val Typography = Typography()
